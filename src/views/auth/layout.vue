ae211f5c25d9ac014cd19804835911f436aa3a0f
Ci <15597293038@163.com>
2024-05-05 14:51:04 +0800

    初始化仓库

---
+++ src/views/auth/layout.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import { isElectronMode } from '@/utils/common'
+</script>
+
+<template>
+  <div id="logo-name" v-if="!isElectronMode()">Lumen IM</div>
+
+  <section class="section">
+    <router-view />
+  </section>
+
+  <div class="copyright">
+    <span>©2024 - 2080 GIM 在线聊天</span>
+    <span><a href="http://beian.miit.gov.cn" target="_blank">菜菜局备2024-5-20号-1</a></span>
+  </div>
+
+  <div class="fly-box">
+    <div class="fly bg-fly-circle1"></div>
+    <div class="fly bg-fly-circle2"></div>
+    <div class="fly bg-fly-circle3"></div>
+    <div class="fly bg-fly-circle4"></div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+body {
+  -webkit-app-region: drag;
+}
+@import '@/assets/css/login.less';
+
+.section {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
